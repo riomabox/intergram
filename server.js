892b6314a5220c567e2c48b5047b489e26abfbe0
Putro Satrio Wibowo <riomabox@gmail.com>
2021-12-27 22:39:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ io.on('connection', function(socket){
 
 function sendTelegramMessage(chatId, text, parseMode) {
     request
-        .post('https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN + '/sendMessage')
+        .post('https://api.telegram.org/bot' + '5029496395:AAGXOlrMK9I7dzd0svt5KYXnVYuQvNlHli4' + '/sendMessage')
         .form({
             "chat_id": chatId,
             "text": text,
